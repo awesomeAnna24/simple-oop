af3c84c89689b195f5a0748b995b26d4353d6c81
Anastacia Muringo <anastasmuringo@gmail.com>
2022-07-21 10:37:40 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,103 @@
+'use strict';
+//Creating parent class
+//Encapsulation & Abstraction
+class SoftwareEngineer {
+  constructor(name, role, position, experience) {
+    this._name = name;
+    this._role = role;
+    this._position = position;
+    this._experience = experience;
+  }
+  //create getter for static properties
+  get name() {
+    return this._name;
+  }
+  get role() {
+    return this._role;
+  }
+
+  //Method to greet user
+  //Polymorphism
+  sayHello() {
+    console.log(
+      `Hello user. This is ${this.name}, a ${this.role} company with ${this._position} with ${this._experience} years of experience in the tech industry.`
+    );
+  }
+}
+
+let company = new SoftwareEngineer(
+  'Ana & Co.',
+  'Full Stack',
+  'junior and senior devs',
+  'Html, css, js, php and react native',
+  10
+);
+
+//Create first child class
+//Inheritance
+class FrontEndDev extends SoftwareEngineer {
+  constructor(name, role, position, languages, experience) {
+    super(name, role, position);
+    this._languages = languages;
+    this._experience = experience;
+  }
+  //Getter for static properties
+  get languages() {
+    return this._languages;
+  }
+  get experience() {
+    return this._experience;
+  }
+  //Polymorphism
+  sayHello() {
+    console.log(
+      `Hello user. I am ${this._name}, a ${this._role} ${this._position} with ${this.experience} years experience and specializing in ${this.languages}.`
+    );
+  }
+}
+//Creating user wit front end dev child class
+let oscar = new FrontEndDev(
+  'Oscar',
+  'Front End Developer',
+  'Junior Dev',
+  'React, Angular, Vue',
+  4
+);
+
+//Create second child class
+class backEndDev extends SoftwareEngineer {
+  constructor(name, role, position, experience, language, previousCo) {
+    super(name, role, position, experience);
+    this._language = language;
+    this._previousCo = previousCo;
+  }
+  //getter
+  get language() {
+    return this._language;
+  }
+  get previousCo() {
+    return this._previousCo;
+  }
+  //sayHello to user
+  sayHello() {
+    console.log(
+      `Hello user. I am ${this._name}, a ${this._role} ${this._position} with ${this._experience} years experience in ${this._language} having previously worked at ${this._previousCo}.`
+    );
+  }
+}
+
+let muringo = new backEndDev(
+  'Muringo',
+  'back end developer',
+  'senior dev',
+  5,
+  'Php and React Native',
+  'Netflix'
+);
+
+//Accessing sayHello from each class
+
+let staff = [company, oscar, muringo];
+for (let devs of staff) {
+  devs.sayHello();
+}
